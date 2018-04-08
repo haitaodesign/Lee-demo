0c89653bcac91914223624e601c594f204f073dc
lihaitao <1775814590@qq.com>
2018-04-08 15:50:35 +0800

    新增 todo应用配置

---
+++ VueToDo/src/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{text}}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "abc"
+    };
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
