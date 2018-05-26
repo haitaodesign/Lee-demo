fa887bbcd627ebf854c20c51f3c189d2843f9686
lihaitao <1775814590@qq.com>
2018-05-26 09:52:35 +0800

    add react-redux

--- make-react-redux/src/Header.js
+++ make-react-redux/src/Header.js
@@ -1,29 +1,42 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { connect } from './react-redux'
 
 class Header extends Component {
-    static contextTypes = {
-        store: PropTypes.object
+    static propTypes = {
+        themeColor: PropTypes.string
     }
-    constructor(){
-        super()
-        this.state = { themeColor: '' }
-    }
+    // static contextTypes = {
+    //     store: PropTypes.object
+    // }
+    // constructor(){
+    //     super()
+    //     this.state = { themeColor: '' }
+    // }
 
-    componentWillMount(){
-        const { store } = this.context
-        this._upadteThemeColor()
-        store.subscribe(() => this._upadteThemeColor())
-    }
-    _upadteThemeColor () {
-        const { store } = this.context
-        const state = store.getState()
-        this.setState({ themeColor: state.themeColor })
-    }
+    // componentWillMount(){
+    //     const { store } = this.context
+    //     this._upadteThemeColor()
+    //     store.subscribe(() => this._upadteThemeColor())
+    // }
+    // _upadteThemeColor () {
+    //     const { store } = this.context
+    //     const state = store.getState()
+    //     this.setState({ themeColor: state.themeColor })
+    // }
     render() {
         return (
-            <h1 style={{ color: this.state.themeColor }}>React.js 小书</h1>
+            <h1 style={{ color: this.props.themeColor }}>React.js 小书</h1>
         );
     }
 }
-export default Header;
+
+const mapStateToProps = (state) => {
+    return {
+        themeColor: state.themeColor
+    }
+}
+
+Header = connect(mapStateToProps)(Header)
+
+export default Header
---
+++ make-react-redux/src/react-redux.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export const connect = (mapStateToProps) => (WrappedComponent)=> {
+    class Connect extends Component {
+        static contextTypes = {
+            store: PropTypes.object
+        }
+        constructor () {
+            super()
+            this.state = { allProps: {} }
+        }
+        componentDidMount () {
+            const { store } = this.context
+            this._updateProps()
+            store.subscribe(() => this._updateProps())
+        }
+        _updateProps () {
+            const { store } = this.context
+            let stateProps = mapStateToProps(store.getState(),this.props)
+            this.setState({
+                allProps:{
+                    ...stateProps,
+                    ...this.props
+                }
+            })
+        }
+        render() {
+            return (
+                <WrappedComponent { ...this.state.allProps }/>
+            )
+        }
+    }
+    return Connect
+}
